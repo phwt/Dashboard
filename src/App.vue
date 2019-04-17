271942c24918912e8c20e4e4afe5764bd57e7624
Kasidis Chaowvasin <seai_kasidis@hotmail.com>
2019-04-17 14:43:15 +0700

    add responsive

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,21 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-4">
-          <h5>Temperature</h5>
+        <div class="col-12 col-md-4">
+          <small>Temperature</small>
           <h2>20</h2>
         </div>
-        <div class="col-4">
-          <h5>Time</h5>
+        <div class="col-12 col-md-4">
+          <small>Time</small>
           <h2>13:00</h2>
         </div>
-        <div class="col-4">
-          <h5>Status</h5>
+        <div class="col-12 col-md-4">
+          <small>Status</small>
           <h2>Connected</h2>
         </div>
       </div>
       <div class="row">
-        <div class="control-card col-6 p-1">
+        <div class="control-card col-12 col-md-6 p-1">
           <div class="row wrapper">
             <div class="col-4">
               <img class="w-100" src="./assets/fan.png" alt="">
@@ -32,7 +32,7 @@
             </div>
           </div>
         </div>
-        <div class="control-card col-6 p-1">
+        <div class="control-card col-12 col-md-6 p-1">
           <div class="row wrapper">
             <div class="col-4">
               <img class="w-100" src="./assets/light.png" alt="">
