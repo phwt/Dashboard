4861be6b4b730faa4f6e24d8cebcc3448a129474
phwt.smwt <phwt.smwt@gmail.com>
2019-06-02 06:59:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <h1>Dashboard</h1>
     <div class="container">
+      <h1>Dashboard</h1>
+      <div class="row">
+
+        <!--กล่องแสดงข้อมูล-->
+        <data-card name="Temperature" path="temperature" suffix="°C" />
+        <data-card name="Humidity" path="humidity" suffix="%" />
+        <data-card id="musichere" name="Music" path="music" />
+        <!--กล่องแสดงข้อมูล-->
+        <audio id="myAudio">
+          <source src="https://www.w3schools.com/tags/horse.ogg" type="audio/ogg">
+          <source src="https://www.w3schools.com/tags/horse.mp3" type="audio/mpeg">
+          Your browser does not support the audio tag.
+        </audio>
+      </div><br>
       <h1>Random Game</h1>
       <hr>
       <br>
@@ -30,66 +43,79 @@
 </template>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
-import DataCard from "@/components/DataCard";
-import { database } from "firebase";
-import ControlCard from "@/components/ControlCard";
-export default {
-  name: "app",
-  components: {
-    DataCard,
-    ControlCard
-  },
-  mounted() {
-    var bgset = ["bg-primary", "bg-danger", "bg-success", "bg-warning"];
-    var bgsets = ["bg-primary", "bg-danger", "bg-success", "bg-warning"];
-    $(document).ready(function() {
-      $("#game-slot").hide();
+  import DataCard from "@/components/DataCard";
+  import {
+    database
+  } from "firebase";
+  import ControlCard from "@/components/ControlCard";
+  export default {
+    name: "app",
+    components: {
+      DataCard,
+      ControlCard
+    },
+    mounted() {
+      var bgset = ["bg-primary", "bg-danger", "bg-success", "bg-warning"];
+      var bgsets = ["bg-primary", "bg-danger", "bg-success", "bg-warning"];
+      $(document).ready(function () {
+        $("#game-slot").hide();
 
-      $("#play-btn").click(function() {
-        $(this).slideUp();
-        $("#game-slot").slideDown();
+        $("#play-btn").click(function () {
+          $(this).slideUp();
+          $("#game-slot").slideDown();
 
-        var runRand = setInterval(() => {
-          $(".circle").each(function(index) {
-            var curbg = bgset[Math.floor(Math.random() * index)];
-            $(this)
-              .removeClass()
-              .addClass("circle " + curbg);
-          });
-        }, 150);
-        var answer = [];
-        setTimeout(() => {
-          clearInterval(runRand);
-          $(".circle").each(function(index) {
-            var curbg = bgset[Math.floor(Math.random() * (4 - index))];
-            if (bgset.indexOf(curbg) > -1) {
-              answer.push(bgsets.indexOf(curbg));
-              bgset.splice(bgset.indexOf(curbg), 1);
-            }
-            $(this)
-              .removeClass()
-              .addClass("circle " + curbg);
-          });
-          console.log("this" + answer);
-          database().ref('game/answer').set(JSON.stringify(answer))
-        }, 1000);
+          var runRand = setInterval(() => {
+            $(".circle").each(function (index) {
+              var curbg = bgset[Math.floor(Math.random() * index)];
+              $(this)
+                .removeClass()
+                .addClass("circle " + curbg);
+            });
+          }, 150);
+          var answer = [];
+          setTimeout(() => {
+            clearInterval(runRand);
+            $(".circle").each(function (index) {
+              var curbg = bgset[Math.floor(Math.random() * (4 - index))];
+              if (bgset.indexOf(curbg) > -1) {
+                answer.push(bgsets.indexOf(curbg));
+                bgset.splice(bgset.indexOf(curbg), 1);
+              }
+              $(this)
+                .removeClass()
+                .addClass("circle " + curbg);
+            });
+            console.log("this" + answer);
+            database().ref('game/answer').set(JSON.stringify(answer))
+          }, 1000);
+        });
+
+        setInterval(() => {
+          if ($("#musichere > h2").text() == "ON") {
+            var audio = new Audio('https://www.w3schools.com/tags/horse.mp3');
+            audio.play();
+          } else {
+            // $("#myAudio").pause();
+          }
+        }, 100);
+
       });
-    });
-  }
-};
+    }
+  };
 </script>
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px 0;
-}
-.circle {
-  height: 50px;
-  width: 50px;
-  border-radius: 50%;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 60px 0;
+  }
+
+  .circle {
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+  }
 </style>
